4c68f0a8fb69f7515bf0368e6ee547f479a80104
Cristiano Oliveira <cristianomoliveira@gmail.com>
2020-05-23 19:49:59 -0300

    mudando titulo da pagina

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     
-    res.render('index');
+    res.render('index',{
+        title:'Minha Loja'
+    });
 
 });
 
