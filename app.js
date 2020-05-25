2c5f471fb51d493953b011b7a8c82d4cef02bd60
Cristiano Oliveira <cristianomoliveira@gmail.com>
2020-05-24 22:35:29 -0300

    adiconando body-parser, session, validator e mensagens

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
 var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
 
 //conectando ao db
 mongoose.connect(config.database);
@@ -21,6 +24,65 @@ app.set('view engine', 'ejs');
 
 //Diretório público
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Add body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+//  cookie: { secure: true }
+}));
+
+// Express Validator middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    },
+    customValidators: {
+        isImage: function (value, filename) {
+            var extension = (path.extname(filename)).toLowerCase();
+            switch (extension) {
+                case '.jpg':
+                    return '.jpg';
+                case '.jpeg':
+                    return '.jpeg';
+                case '.png':
+                    return '.png';
+                case '':
+                    return '.jpg';
+                default:
+                    return false;
+            }
+        }
+    }
+}));
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+
+
+
+
 
 //Setar as rotas
 var pages = require('./routes/pages.js');
